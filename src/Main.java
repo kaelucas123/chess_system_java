a077d106e1e33775f2e103911a88cfce9d23fbe7
Kae Lucas <kaeteodoro@gmail.com>
2023-01-20 15:28:38 -0300

    Added classes: board and piece

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position pos = new Position(1,5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
+        
     }
 }
---
+++ src/boardgame/Board.java
@@ -0,0 +1,23 @@
+package boardgame;
+
+public class Board {
+    private int rows;
+    private int collumns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int collumns) {
+        this.rows = rows;
+        this.collumns = collumns;
+        pieces = new Piece[rows][collumns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCollumns() {
+        return collumns;
+    }
+
+
+}
