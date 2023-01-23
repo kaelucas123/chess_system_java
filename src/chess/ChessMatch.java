5307e4cab07fd5a2555f2f6f4ae63885826cd389
Kae Lucas <kaeteodoro@gmail.com>
2023-01-23 14:32:51 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -94,7 +94,8 @@ public class ChessMatch {
         return (ChessPiece) capturePiece;
     }
     private void undoMove(Position source, Position target, Piece capturedPiece){
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if(capturedPiece != null){
@@ -104,7 +105,8 @@ public class ChessMatch {
         }
     }
     private Piece makeMove(Position source, Position target){
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
 
