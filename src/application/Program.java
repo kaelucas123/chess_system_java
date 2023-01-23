49077b162daaf8f02a81d8588dbc87b5f47e9d03
Kae Lucas <kaeteodoro@gmail.com>
2023-01-23 07:46:56 -0300

    Moving pieces

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+    }
+
+}
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,6 +34,18 @@ public class Board {
         }
         return pieces[position.getRow()][position.getColumn()];
     }
+    public Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
     public void placePiece(Piece piece, Position position){
         if(thereIsAPiece(position)){
             throw new BoardException("There is already a piece on position " + position);
